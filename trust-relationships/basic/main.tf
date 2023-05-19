d0566976eeca2fd738d5ff2ed84b680f309fb15d
Robin Norwood <rnorwood@hashicorp.com>
2023-05-19 15:07:13 -0500

    Add TFE configuration

--- trust-relationships/basic/main.tf
+++ trust-relationships/basic/main.tf
@@ -5,6 +5,10 @@ provider "aws" {
   region = var.aws_region
 }
 
+provider "tfe" {
+  hostname = var.tfc_hostname
+}
+
 data "tls_certificate" "tfc_certificate" {
   url = "https://${var.tfc_hostname}"
 }
@@ -63,4 +67,41 @@ EOF
 resource "aws_iam_role_policy_attachment" "tfc_policy_attachment" {
   role       = aws_iam_role.tfc_role.name
   policy_arn = aws_iam_policy.tfc_policy.arn
+}
+
+data "tfe_oauth_client" "client" {
+  organization = var.tfc_organization_name
+  name = var.vcs_oath_client_name
+  service_provider = var.vcs_service_provider
+}
+
+resource "tfe_workspace" "trusted_workspace" {
+  name         = var.tfc_workspace_name
+  organization = var.tfc_organization_name
+  working_directory = var.vcs_working_directory
+
+  vcs_repo {
+    identifier = var.vcs_identifier
+    oauth_token_id = data.tfe_oauth_client.client.oauth_token_id
+  }
+}
+
+resource "tfe_variable" "enable_aws_provider_auth" {
+  workspace_id = tfe_workspace.trusted_workspace.id
+
+  key      = "TFC_AWS_PROVIDER_AUTH"
+  value    = "true"
+  category = "env"
+
+  description = "Enable the Workload Identity integration for AWS."
+}
+
+resource "tfe_variable" "tfc_aws_role_arn" {
+  workspace_id = tfe_workspace.trusted_workspace.id
+
+  key      = "TFC_AWS_RUN_ROLE_ARN"
+  value    = aws_iam_role.tfc_role.arn
+  category = "env"
+
+  description = "The AWS role arn runs will use to authenticate."
 }
