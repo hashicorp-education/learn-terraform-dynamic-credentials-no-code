dc66c60f2a1f0844c5beef7dfab82797927cdbc6
Robin Norwood <rnorwood@hashicorp.com>
2023-05-26 15:58:03 -0500

    Add outputs for role ARN and policy

--- trust-relationships/vault-backed/outputs.tf
+++ trust-relationships/vault-backed/outputs.tf
@@ -3,6 +3,16 @@ output "openid_claims" {
   value       = vault_jwt_auth_backend_role.tfc_role.bound_claims
 }
 
+output "role_arn" {
+  description = "ARN for trust relationship role"
+  value       = aws_iam_role.tfc_role.arn
+}
+
+output "policy" {
+  description = "Policy for this trust relationship"
+  value = aws_iam_policy.tfc_policy.policy
+}
+
 # output "tfc_workspace_name" {
 #   description = "Name of configured workspace"
 #   value       = tfe_workspace.trust_workspace.name
